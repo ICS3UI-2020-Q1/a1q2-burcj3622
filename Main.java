bcc6a971529df113f5b7bc378ac437771adb24bc
burcj3622 <burcj3622@wrdsb.ca>
2020-09-14 15:31:47 +0000

    division

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-/**
- *
- * @author 
- */
+import java.util.Scanner;
+ 
 public class Main {
 
   /**
@@ -10,6 +8,19 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
-  }
+   Scanner input = new Scanner(System.in);
+  
+   //asks for number to divivde
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+  
+  // Variable A is one number needed for division
+  int A = input.nextInt();
+
+  //variable B is the second number needed for division 
+  int B = input.nextInt();
+
+// declare and calculate the division
+System.out.println( A+ "/" + B +" is " + A / B + " with a remainder of " + A % B );
 }
+}
+
